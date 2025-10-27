9f3b8820da5580c3ab8a162ded404096e2026752
gtbu <manbu@mail.de>
2025-10-27 19:41:06 +0100

    Update coco
    solution for without : short_open_tag=On

--- plugins/coco/views/info.php
+++ plugins/coco/views/info.php
@@ -14,7 +14,7 @@ if (!defined("CMSIMPLE_XH_VERSION")) {header("HTTP/1.0 403 Forbidden"); exit;}
 <h1>Coco <?=$this->esc($version)?></h1>
 <div class="coco_syscheck">
   <h2><?=$this->text('syscheck_title')?></h2>
-<?foreach ($checks as $check):?>
-  <p class="<?=$this->esc($check->class)?>"><?=$this->esc($check->message)?></p>
-<?endforeach?>
+<?php foreach ($checks as $check): ?>
+  <p class="<?=$this->esc($check?->class)?>"><?=$this->esc($check?->message)?></p>
+<?php endforeach; ?>
 </div>
