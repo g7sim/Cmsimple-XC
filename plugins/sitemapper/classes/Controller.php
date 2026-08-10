bcb48dc3e203d6f190a76b749384c1f56d558915
gtbu <30436307+gtbu@users.noreply.github.com>
2026-08-10 13:40:29 +0200

    modiefied helpf files for sitemapper

--- plugins/sitemapper/classes/Controller.php
+++ plugins/sitemapper/classes/Controller.php
@@ -3,6 +3,7 @@
 /**
  * @copyright 2011-2017 Christoph M. Becker <http://3-magi.net/>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * modified 8/2026 by github.com/g7sim
  */
 
 namespace Sitemapper;
